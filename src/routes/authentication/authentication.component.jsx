7ba80df45c98ef9ee43749d3db2908ca72268c01
Kapil Thapa <124021602+kapiltha9a@users.noreply.github.com>
2023-07-12 01:51:54 -0400

    authentication clean-up and styling completed

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,36 +1,10 @@
-import { useEffect } from "react";
-import { getRedirectResult } from 'firebase/auth';
-
-import { auth, sighInWithGooglePopup, sighInWithGoogleRedirect, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
-
+import './Authentication.styles.scss';
 
 const Authentication = () => {
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await getRedirectResult(auth);
-      if (response) {
-        const userDocRef = await createUserDocumentFromAuth(response.user);
-      }
-    }
-    fetchData();
-  }, []);
-
-  
-
-  // const logGoogleRedirectUser = async () => {
-  //   const { user } = await sighInWithGoogleRedirect();
-  //   console.log({ user });
-  // };
-
   return (
-    <div>
-      <h1>Sign In Page</h1>
-      {/* <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={sighInWithGoogleRedirect}>Sign in with Google Redirect</button>
-      <br/> */}
+    <div className='authentication-container'>
       <SignInForm />
       <SignUpForm />
     </div>
