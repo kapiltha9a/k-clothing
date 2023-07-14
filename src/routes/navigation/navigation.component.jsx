653364bdf3c4c7b071e4058c252c0c3b381cf8b0
Kapil Thapa <kapil.thapa@towercompanies.com>
2023-07-14 16:54:25 -0400

    added cart-icon

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,7 @@ import { useContext } from 'react';
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
 import './navigation.styles.scss';
 
 const Navigation = () => {
@@ -29,7 +30,7 @@ const Navigation = () => {
                             </Link>     
                         )
                     }
-                    
+                    <CartIcon />                    
                 </div>
             </div>
             <Outlet />
