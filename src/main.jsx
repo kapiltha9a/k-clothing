5aaf42f15dd40962ef2e21e4427522f63eed3712
Kapil Thapa <kapil.thapa@towercompanies.com>
2023-07-14 16:19:31 -0400

    added productrs & product card to the shop

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx';
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context.jsx';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>       {/* // react router */}
-      <UserProvider>      {/* // context  */}
-        <App />
+    <BrowserRouter>           {/* // react router */}
+      <UserProvider>          {/* // context  */}
+        <ProductsProvider>    {/* // context  */}
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
