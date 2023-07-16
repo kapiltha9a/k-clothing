af2c81a69d978e6b72e62e69b9f336d24b66c743
Kapil Thapa <124021602+kapiltha9a@users.noreply.github.com>
2023-07-16 00:05:42 -0400

    adding cart context and shopping icon as well as cart dropdown

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from './contexts/products.context.jsx';
+import { CartProvider } from './contexts/cart.context.jsx';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>           {/* // react router */}
       <UserProvider>          {/* // context  */}
         <ProductsProvider>    {/* // context  */}
-          <App />
+          <CartProvider>      {/* // context  */}
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,12 +3,15 @@ import { useContext } from 'react';
 
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import './navigation.styles.scss';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <>
@@ -32,6 +35,7 @@ const Navigation = () => {
                     }
                     <CartIcon />                    
                 </div>
+                {isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </>
